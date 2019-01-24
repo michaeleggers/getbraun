ddc8f4c9c3a3cfb9dc17d2408dd9aebc15c64132
Michael Eggers <eggers@hm.edu>
2019-01-24 15:00:35 +0100

    initial commit: python script downloads and creates folders for lectures from Prof. Brauns's website.

---
+++ get.py
@@ -0,0 +1,54 @@
+# Get material from Braun's lectures.
+# Written by the KING OF PYTHON himself.
+
+# TODOs:
+# - check if json object is available
+# - make CLI for what to query
+
+import urllib.request
+import ssl
+import json
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description="download lecture data from ob.cs.hm.edu")
+parser.add_argument("lecturename", metavar="lecture", type=str, help="lecturename to download lecture-data from")
+args = parser.parse_args()
+
+lecturename = args.lecturename # change this to get stuff from another lecture
+baseURL = "https://ob.cs.hm.edu/api/base.json"
+artifactURL = "https://ob.cs.hm.edu/api/lectures/" + lecturename + "/base.json"
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+baseURLresponse = urllib.request.urlopen(baseURL, context=ctx).read()
+artifactURLresponse = urllib.request.urlopen(artifactURL, context=ctx).read()
+
+
+baseJSON = json.loads(baseURLresponse)
+artifactJSON = json.loads(artifactURLresponse)
+
+filesPrefix = baseJSON["filesPrefix"]
+artifactDirs = baseJSON["dirs"]
+
+# add artifacts here:
+artifacts = ["slides", "exercises"]
+
+for artifact in artifacts:
+	artifactToGet = artifactDirs[artifact]
+	suffix = baseJSON["suffixes"][artifactToGet]
+	files = artifactJSON["maincontent"][artifactToGet]
+	for file in files:
+		f = file["file"]
+		print(f)
+		fullpath = filesPrefix + "/" + lecturename + "/" + artifactToGet + "/" + f + "." + suffix
+		print(fullpath)
+		content = urllib.request.urlopen(fullpath, context=ctx)
+		if not os.path.exists(os.path.join(lecturename, artifactToGet)):
+			os.makedirs(os.path.join(lecturename,artifactToGet))
+		fileOnDisk = open(lecturename + "/" + artifactToGet + "/" + f + "." + suffix, mode="wb")
+		fileOnDisk.write(content.read())
+#print(filesPrefix)
+#print(suffix)
